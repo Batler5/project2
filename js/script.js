73388937e28f756b7326651cd0bcc9c39052bc73
Daniil <danikshender6@gmail.com>
2023-02-21 21:20:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+"use strict";
+
+
+
+// console.log("arr"+" - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+//     // ++incr;
+//     // --decr;
+//     console.log(5113%25);
+
+//     console.log(2+2*2!='6');
+    
+//     const isChecked = false,
+//     isClose = false;
+//     console.log(isChecked || !isClose);
+
+
+    // const answers = [];
+
+// answers[0] = prompt('Ваше имя', '');
+// answers[1] = prompt('Ваш возраст', '');
+// answers[2] = prompt('Ваш вес', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+// const answer = +prompt("Вам есть 18 ?", "18");
+// console.log((answer)+5);
+
+// const result = confirm("Are you here ?");
+// console.log(result);
+
+
+// alert('Hello mf');
+
+
+
+
+// const obj = {
+// name : "John",
+// age: 25,
+// isMarried:false
+// };
+
+//  console.log(obj ['name']);
+// let arr = ['plum.png', 'orange.jpeg', 'apple.bmp',6 , {fal: true}, [false, true, false]];
+
+// console.log(arr[3]);
+
+// let a = 4.6;
+// console.log(-4/0);
+// console.log('string'*9);
+
+// const persone = "Alex";
+
+// const bool = false;
+
+// let something;
+// console.log(something);
+
+// console.log(39247);
+// let number = 7;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const Obj = {
+// a:50
+
+// }
+// Obj.a=10;
+// console.log(Obj);
+
+
+// var name = 'Ivan'; name = 'Alex';console.log(name);
+
+
+// {
+
+//     var result = 50;
+// }
+// console.log(result);
+
+// alert(5);
+// [].push('a');
