1f177d9f36a02b0938980b2c97a218757c2422f7
Daniil <danikshender6@gmail.com>
2023-02-23 21:23:16 +0300

    up5

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 "use strict"
-
-let num = 50;
-
-while (num < 55){
-    console.log(num);
-    num++;
+console.log(Calc(10, 20));
+function Calc(i, j){
+    
+    let num = i+j;
+    return num;
 }
+const logger = function(){
+console.log('hello');
+};
+logger();
 
-do {
-    console.log(num);
-    num--;
-}
-while (num > 50)
+const calc = (a,b)=> {return a+b};
+console.log(calc(4,5));
+// let num = 50;
 
-for(let i=1; i< 10; i++){
-if (i===6){
-    // break;
-    continue;
-}
-console.log(i);
-}
+// while (num < 55){
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num--;
+// }
+// while (num > 50)
+
+// for(let i=1; i< 10; i++){
+// if (i===6){
+//     // break;
+//     continue;
+// }
+// console.log(i);
+// }
 
 
- num = 500;
-if (num < 49){
-    console.log('error !');
-} else if (num > 100){
-    console.log('more !');
-} else console.log("ok !");
+//  num = 500;
+// if (num < 49){
+//     console.log('error !');
+// } else if (num > 100){
+//     console.log('more !');
+// } else console.log("ok !");
 
-(num === 50) ? console.log("ok !"): console.log('error !');
+// (num === 50) ? console.log("ok !"): console.log('error !');
 
-num = '50';
+// num = '50';
 
-switch (num) {
-    case '49': console.log('Less !');
-    break;
-    case '50': console.log('OK !');
-    break;
-    case '100': console.log('error !(More)');
-    break;
-}
+// switch (num) {
+//     case '49': console.log('Less !');
+//     break;
+//     case '50': console.log('OK !');
+//     break;
+//     case '100': console.log('error !(More)');
+//     break;
+// }
