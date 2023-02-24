40e1f84c6417c918652d76243f8d316b5274fb98
Daniil <danikshender6@gmail.com>
2023-02-24 19:23:22 +0300

    up6

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 "use strict"
 
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('test');
+    }
+};
+
+// options.makeTest();
+// console.log(Object.keys(options).length);
+
+const {border, bg} = options.colors;
+const {width}=options;
+console.log(width);
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+// let counter=0;
+// for (let key in options){
+//     if(typeof(options[key])==='object'){
+//         for(let i in options[key]){
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        
+//     }
+// counter++;
+// }
+// console.log(counter);
+//learnJS('JavaScript', first);
+function first(){
+    console.log('я прошел этот урок');
+}
+function learnJS(lang, callback){
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
 // let str = "test/FEED";
 // let arr = [1,4,9,3,2,43,23];
 // str.toLocaleUpperCase();
