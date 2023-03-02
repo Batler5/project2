1be063e36604cf518ce572b7058d744bf3eeaca6
Batler5 <115727657+Batler5@users.noreply.github.com>
2023-03-02 21:45:17 +0300

    up

--- js/script.js
+++ js/script.js
@@ -25,29 +25,52 @@ const movieDB = {
 };
 
 
-const adverts = document.querySelectorAll('.promo__adv img');
-adverts.forEach(items=>{
-items.remove();
+const adv = document.querySelectorAll('.promo__adv img');
+adv.forEach(item=>{
+item.remove();
 });
 
+const genre=document.querySelector('.promo__genre');
+genre.textContent='Драма';
 
-const poster = document.querySelector('.promo__bg'),
-    genre = poster.querySelector('.promo__genre'),
-    interActive = document.querySelector('.promo__interactive-list');
+const poster = document.querySelector('.promo__bg');
+poster.style.backgroundImage = 'url(img/bg.jpg)';
 
-genre.textContent='ДРАМА';
-
-
-poster.style.backgroundImage = 'url(img/bg.jpg)'
-
-interActive.innerHTML = "";
 movieDB.movies.sort();
-movieDB.movies.forEach((film, i)=>{
-interActive.innerHTML +=`
-<li class="promo__interactive-item">${i+1} ${film}
-    <div class="delete"></div>
-    </li>
-`
 
+const inter = document.querySelector('.promo__interactive-list');
+inter.innerHTML="";
+movieDB.movies.forEach((films, i)=>{
+     inter.innerHTML += `
+         <li class="promo__interactive-item">${i+1} ${films}
+         <div class="delete"></div>
+         </li>
+     `    
 });
+
+// const adverts = document.querySelectorAll('.promo__adv img');
+// adverts.forEach(items=>{
+// items.remove();
+// });
+
+
+// const poster = document.querySelector('.promo__bg'),
+//     genre = poster.querySelector('.promo__genre'),
+//     interActive = document.querySelector('.promo__interactive-list');
+
+// genre.textContent='ДРАМА';
+
+
+// poster.style.backgroundImage = 'url(img/bg.jpg)'
+
+// interActive.innerHTML = "";
+// movieDB.movies.sort();
+// movieDB.movies.forEach((film, i)=>{
+// interActive.innerHTML +=`
+// <li class="promo__interactive-item">${i+1} ${film}
+//     <div class="delete"></div>
+//     </li>
+// `
+
+// });
 
